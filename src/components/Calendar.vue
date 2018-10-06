03f089ac48a02befe8efeda0b9e3b79677f1bcc6
Carlos Azaustre <cazaustre@gmail.com>
2018-10-06 21:46:36 +0200

    add google calendar as vue component

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,9 +1,28 @@
 <template>
-  <h2>Calendar</h2>
+  <div>
+    <h2>Calendar</h2>
+    <full-calendar :config="config" :events="events"></full-calendar>
+  </div>
 </template>
 
 <script>
+import 'fullcalendar/dist/fullcalendar.css';
+import 'fullcalendar/dist/locale/es';
+import 'fullcalendar/dist/gcal';
+
 export default {
   name: 'calendar',
+
+  data: () => ({
+    config: {
+      locale: 'es',
+      googleCalendarApiKey: 'AIzaSyCfbXIvFdvtYLLFi8vtvxjZSSWuQc7GQpA',
+      events: {
+        googleCalendarId: 'tqrt63ftjinu72qtv4elb00lcs@group.calendar.google.com',
+      },
+      defaultView: 'month',
+    },
+    events: [],
+  }),
 };
 </script>
